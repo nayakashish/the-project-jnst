f13fe947d71db943b1d8f8ab4eca66605abf8121
Saad <saadtab01@gmail.com>
2024-11-15 22:19:59 -0800

    added some notes on how to run the server

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+# this is the server
 # use "pip install flask" to install flask, which is the python framework to run a server 
-from flask import Flask
+# to run, type in python app.py in the terminal (dont forget to be in the project) 
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
+
+# OpenWeather API key (To be added)
+API_KEY = ""
 
 @app.route("/")
 def home():

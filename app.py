371f546e8938bbdd47f3dd58f585568b2cbd3bb3
Saad <saadtab01@gmail.com>
2024-11-16 18:10:37 -0800

    added OPW API key into git ignore file

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 # this is the server
 # use "pip install flask" to install flask, which is the python framework to run a server 
 # to run, type in python app.py in the terminal (dont forget to be in the project) 
+import os
 from flask import Flask, render_template, request, jsonify
 import requests
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 # OpenWeather API key (To be added)
-API_KEY = ""
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
 
 @app.route("/")
 def home():

74c69e460e0793139aea7a32d3e737cd859a1adb
Saad <saadtab01@gmail.com>
2024-11-16 18:31:08 -0800

    made a comment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # this is the server
 # use "pip install flask" to install flask, which is the python framework to run a server 
-# to run, type in python app.py in the terminal (dont forget to be in the project) 
+# to run, type in python app.py in the terminal (dont forget to be in the project directory) 
 import os
 from flask import Flask, render_template, request, jsonify
 import requests

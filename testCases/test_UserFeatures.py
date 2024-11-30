094d7d2e8df41819cfa011379895d22125ab3395
thndlovu <tndlovu@student.ubc.ca>
2024-11-29 21:37:53 -0800

    Added test case for viewing weather upon app launch.

--- testCases/test_UserFeatures.py
+++ testCases/test_UserFeatures.py
@@ -7,3 +7,10 @@ def client():
     with app.test_client() as client:
         yield client
 
+def test_view_weather_on_open(client):
+    """
+    Test that the app allows users to view weather when connected to a network.
+    """
+    response = client.get("/") #Sends a GET request to the route ("/") of the app
+    assert response.status_code == 200 #Checks if the HTTP request code is 200 (OK), indicating that server succesfully processed the request
+    assert b"Current Weather" in response.data #Verifies that the response contains "Current Weather" text

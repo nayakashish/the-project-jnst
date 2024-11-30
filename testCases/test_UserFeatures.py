c4bbc6913e2e6398ecf4ed432baba78e501829c9
thndlovu <tndlovu@student.ubc.ca>
2024-11-29 21:39:50 -0800

    Added test case for viewing 5-day weather forecast.

--- testCases/test_UserFeatures.py
+++ testCases/test_UserFeatures.py
@@ -14,3 +14,13 @@ def test_view_weather_on_open(client):
     response = client.get("/") #Sends a GET request to the route ("/") of the app
     assert response.status_code == 200 #Checks if the HTTP request code is 200 (OK), indicating that server succesfully processed the request
     assert b"Current Weather" in response.data #Verifies that the response contains "Current Weather" text
+
+def test_view_five_day_forecast(client):
+    """
+    Test that the app provides a 5 day weather forecast.
+    """
+    response = client.get("/") #Sends a GET request to the "/forecast" route
+    assert response.status_code == 200
+    assert b"5-Day Forecast" in response.data #Verifies that the response contains "5-Day Forecast" text
+
+    

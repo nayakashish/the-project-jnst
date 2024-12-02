ef0f3a6ef428b563bacc3f7d32235b22532b9e11
thndlovu <tndlovu@student.ubc.ca>
2024-12-02 01:29:49 -0800

    Added test case to verify retrieval of current weather data using the external API.

--- testCases/test_WeatherDataAPI.py
+++ testCases/test_WeatherDataAPI.py
@@ -7,3 +7,11 @@ def client():
     with app.test_client() as client:
         yield client
 
+def test_retrieve_current_weather(client):
+    """
+    Test that the app retreives current weather data using the external API.
+    """
+    response = client.get("/api/weather?city=Kelowna") #Sends GET request to retrieve weather data for Vancouver.
+    #Checks if the HTTP request code is 200 (OK), indicating that the server succesfully processed the request
+    assert response.status_code == 200 
+    assert "temperature" in response.json #Verifies that the JSON response contains a temperature field. 
